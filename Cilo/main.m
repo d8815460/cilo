8078af917e4eba911b7527d824e195a94b7c09f5
Ayi <d8815460@yahoo.com.tw>
2015-08-19 19:52:08 +0800

    Initial Commit

---
+++ Cilo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cilo
+//
+//  Created by 駿逸 陳 on 2015/8/19.
+//  Copyright (c) 2015年 駿逸 陳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
